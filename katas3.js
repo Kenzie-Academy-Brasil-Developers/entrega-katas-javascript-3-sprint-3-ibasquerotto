5d831ac1d9f5b90240425fb9833390c5a2528715
Igor Basquerotto <basquerotto@gmail.com>
2021-07-15 10:20:25 -0300

    Corrigindo o retorno das funcoes

--- katas3.js
+++ katas3.js
@@ -9,39 +9,54 @@ function show(id, resultado){
 
 function kata1() {
     // implemente o código do kata 1 aqui
+    let retorno = []
     for (let i = 1; i <= 25; i++){
     
         let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
         show("kata1", li)
+       
         }
-      }
-      
+        console.log(retorno)
+        return retorno
+        
+      } 
+   
       kata1()
 
 function kata2() {
     // implemente o código do kata 2 aqui
+    let retorno = []
     for (let i = 25; i >= 1; i--){
     
         let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
         show("kata2", li)
         }
+        console.log(retorno)
+        return retorno
       }
       
       kata2()
 
 function kata3() {
     // implemente o código do kata 3 aqui
+    let retorno = []
     for (let i = -1; i >= -25; i--){
     
         let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
         show("kata3", li)
         }
+        console.log(retorno)
+        return retorno
+
       }
       
       kata3()
@@ -49,13 +64,17 @@ function kata3() {
 
 function kata4() {
     // implemente o código do kata 4 aqui
+    let retorno = []
     for (let i = -25; i <= -1; i++){
     
         let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
         show("kata4", li)
         }
+        console.log(retorno)
+        return retorno
       }
       
       kata4()
@@ -64,14 +83,18 @@ function kata4() {
 
 function kata5() {
     // implemente o código do kata 5 aqui
+    let retorno = []
     for (let i = 25; i >= -25; i--){
         if (i%2 != 0 ) {
             let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
         show("kata5", li)
             }
         }
+        console.log(retorno)
+        return retorno
       }
       
       kata5()
@@ -79,14 +102,18 @@ function kata5() {
 
 function kata6() {
     // implemente o código do kata 6 aqui
+    let retorno = []
     for (let i = 3; i <= 100; i++){
         if (i%3 == 0 ) {
             let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
             show("kata6", li)
             }
         }
+        console.log(retorno)
+        return retorno
       }
       
       kata6()
@@ -94,28 +121,36 @@ function kata6() {
 
 function kata7() {
     // implemente o código do kata 7 aqui
+    let retorno = []
     for (let i = 7; i <= 100; i++){
         if (i%7 == 0 ) {
             let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
             show("kata7", li)
             }
         }
+        console.log(retorno)
+        return retorno
       }
       
       kata7()
 
 function kata8() {
     // implemente o código do kata 8 aqui
+    let retorno = []
     for (let i = 100; i >= 1; i--){
         if (i % 3 == 0  || i % 7 == 0) {
             let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
             show("kata8", li)
             }
         }
+        console.log(retorno)
+        return retorno
       }
       
       kata8()
@@ -123,14 +158,18 @@ function kata8() {
 
 function kata9() {
     // implemente o código do kata 9 aqui
+    let retorno = []
     for (let i = 5; i <= 100; i++){
         if (i % 2 != 0  && i % 5 == 0) {
             let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = i;
+            retorno.push(i)
             show("kata9", li)
             }
         }
+        console.log(retorno)
+        return retorno
       }
       
       kata9()
@@ -138,13 +177,16 @@ function kata9() {
 
 function kata10() {
     // implemente o código do kata 10 aqui
+    let retorno = sampleArray
     for (let i = 0; i < sampleArray.length; i++){
     
-            let li = document.createElement("li");
-                li.className = "listNumber"
-                li.innerHTML = sampleArray[i];
+        let li = document.createElement("li");
+            li.className = "listNumber"
+            li.innerHTML = sampleArray[i];
             show("kata10", li)
-            }
+        }
+        console.log(retorno)
+        return retorno
       }
       
       kata10()
@@ -152,14 +194,20 @@ function kata10() {
 
 function kata11() {
     // implemente o código do kata 11 aqui
+    let retorno = []
+    let saida = 0
     for (let i = 0; i < sampleArray.length; i++){
         if (sampleArray[i] % 2 == 0){
             let li = document.createElement("li");
                 li.className = "listNumber"
                 li.innerHTML = sampleArray[i];
+                saida = sampleArray[i]
+                retorno.push(saida)
             show("kata11", li)
         }
     }
+    console.log(retorno)
+        return retorno
   }
   
   kata11()
@@ -168,47 +216,66 @@ function kata11() {
 
 function kata12() {
     // implemente o código do kata 12 aqui
+    let retorno = []
+    let saida = 0
     for (let i = 0; i < sampleArray.length; i++){
         if (sampleArray[i] % 2 != 0){
             let li = document.createElement("li");
                 li.className = "listNumber"
                 li.innerHTML = sampleArray[i];
+                saida = sampleArray[i]
+                retorno.push(saida)
             show("kata12", li)
         }
     }
+    console.log(retorno)
+        return retorno
   }
   
   kata12()
 
 function kata13() {
     // implemente o código do kata 13 aqui
+    let retorno = []
+    let saida = 0
     for (let i = 0; i < sampleArray.length; i++){
         if (sampleArray[i] % 8 == 0 ){
             let li = document.createElement("li");
                 li.className = "listNumber"
                 li.innerHTML = sampleArray[i];
+                saida = sampleArray[i]
+                retorno.push(saida)
             show("kata13", li)
         }
     }
+    console.log(retorno)
+        return retorno
   }
   
   kata13()
 
 function kata14() {
     // implemente o código do kata 14 aqui
+    let retorno = []
+    let saida = 0
     for (let i = 0; i < sampleArray.length; i++){
     
         let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = sampleArray[i] * sampleArray[i];
+            saida = sampleArray[i] * sampleArray[i]
+            retorno.push(saida)
         show("kata14", li)
         }
+        console.log(retorno)
+        return retorno
   }
   
   kata14()
 
 function kata15() {
     // implemente o código do kata 15 aqui
+    let retorno = []
     let soma = 0
     for (let i = 1; i <= 20; i++){
         soma = soma + i 
@@ -218,7 +285,10 @@ function kata15() {
         li.className = "listNumber"
         
         li.innerHTML = soma;
+        retorno.push(soma)
         show("kata15", li)
+        console.log(retorno)
+        return retorno
         
       }
       
@@ -227,6 +297,7 @@ function kata15() {
 
 function kata16() {
     // implemente o código do kata 16 aqui
+    let retorno = []
     let soma = 0
     for (let i = 0; i < sampleArray.length; i++){
         soma = soma + sampleArray[i] 
@@ -236,7 +307,11 @@ function kata16() {
         li.className = "listNumber"
         
         li.innerHTML = soma;
+        retorno.push(soma)
         show("kata16", li)
+        
+        console.log(retorno)
+        return retorno
         
       }
       
@@ -244,6 +319,7 @@ function kata16() {
 
 
 function kata17() {
+    let retorno = []
     let menorNumero = sampleArray[0]
     // implemente o código do kata 17 aqui
     for (let i = 1; i < sampleArray.length; i++){
@@ -254,7 +330,11 @@ function kata17() {
         let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = menorNumero;
-        show("kata17", li)
+            retorno.push(menorNumero)
+            show("kata17", li)
+
+            console.log(retorno)
+            return retorno
         }
       
       
@@ -262,6 +342,7 @@ function kata17() {
 
 
 function kata18() {
+    let retorno = []
     let maiorNumero = sampleArray[0]
     // implemente o código do kata 17 aqui
     for (let i = 1; i < sampleArray.length; i++){
@@ -272,7 +353,11 @@ function kata18() {
         let li = document.createElement("li");
             li.className = "listNumber"
             li.innerHTML = maiorNumero;
-        show("kata18", li)
+            retorno.push(maiorNumero)
+            show("kata18", li)
+
+            console.log(retorno)
+             return retorno
         }
       
       
